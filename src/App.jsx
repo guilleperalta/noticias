19a730d9661280fbbdc4f5075cdc48a9b648d5f2
guilleperalta <guille.peralta.dev@gmail.com>
2023-10-07 13:09:28 -0300

    Se deja el proyecto listo, funcionando

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { Container, Grid, Typography } from '@mui/material'
+import Formulario from './components/Formulario'
+import { NoticiasProvider } from './context/NoticiasProvider'
+import ListadoNoticias from './components/ListadoNoticias'
+import React from 'react'
+
+const App = () => {
+  return (
+    <NoticiasProvider>
+      <Container>
+
+        <header>
+          <Typography align='center' marginY={5} component='h1' variant='h3'>
+            Buscador de noticias
+          </Typography>
+        </header>
+
+        <Grid 
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid 
+            item 
+            xs={12}
+            md={6}
+          >
+            <Formulario />
+          </Grid>
+        </Grid>
+
+        <ListadoNoticias />
+
+      </Container>
+    </NoticiasProvider>
+  )
+}
+
+export default App
