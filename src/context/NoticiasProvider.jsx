417fefda2d1b26a41fc6c4ef76d4021f6406ea11
guilleperalta <guille.peralta.dev@gmail.com>
2023-10-07 13:18:21 -0300

    Se cambia el pais, se deja en Argentina

--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -11,7 +11,7 @@ const NoticiasProvider = ({children}) => {
 
     useEffect(() => {
         const consultarAPI = async () => {
-            const url = `https://newsapi.org/v2/top-headlines?country=us&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`
+            const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`
             const {data} = await axios(url)
             setNoticias(data.articles)
             setTotalNoticias(data.totalResults)
@@ -22,10 +22,11 @@ const NoticiasProvider = ({children}) => {
 
     useEffect(() => {
         const consultarAPI = async () => {
-            const url = `https://newsapi.org/v2/top-headlines?country=us&page=${pagina}&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`
+            const url = `https://newsapi.org/v2/top-headlines?country=ar&page=${pagina}&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`
             const {data} = await axios(url)
             setNoticias(data.articles)
             setTotalNoticias(data.totalResults)
+            console.log(data)
         }
         consultarAPI()
     },[pagina])
